268856858eb2850a3c5450f83a19ab0ecf0b5859
Sebastian <sebassuarez103@gmail.com>
2025-09-27 00:14:53 -0500

    Configuración de application.properties y perfiles en Spring Boot

--- src/main/java/com/streaming_play/HelloController.java
+++ src/main/java/com/streaming_play/HelloController.java
@@ -1,19 +1,21 @@
 package com.streaming_play;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+    private final String platform;
     private final StreamingPlayAiService aiService;
 
-    public HelloController(StreamingPlayAiService aiService) {
+    public HelloController(StreamingPlayAiService aiService, @Value("${spring.application.name}")String platform) {
         this.aiService = aiService;
+        this.platform = platform;
     }
 
     @GetMapping("/")
     public String hello() {
-        return this.aiService.generateGreeting();
+        return this.aiService.generateGreeting(platform);
     }
 }
--- src/main/java/com/streaming_play/StreamingPlayAiService.java
+++ src/main/java/com/streaming_play/StreamingPlayAiService.java
@@ -1,14 +1,15 @@
 package com.streaming_play;
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService
 public interface StreamingPlayAiService {
 
     @UserMessage("""
-            Genera un saludo de bienvenida a la plataforma de Streaming Play.
+            Genera un saludo de bienvenida a la plataforma de Gestion de Películas {{platform}}.
             Usa menos de 120 caracteres.
             """)
-    String generateGreeting();
+    String generateGreeting(@V("platform") String platform);
 }
