05a31de0b0f6da5422054d219b148c25f65df02e
Sebastian <sebassuarez103@gmail.com>
2025-10-04 14:42:04 -0500

    Add MovieEntityRepository and MovieMapper for movie data handling

---
+++ src/main/java/com/streaming/play/persistence/mapper/MovieMapper.java
@@ -0,0 +1,20 @@
+package com.streaming.play.persistence.mapper;
+
+import com.streaming.play.domain.dto.MovieDto;
+import com.streaming.play.persistence.entity.MovieEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface MovieMapper {
+
+    @Mapping(source = "titulo", target = "title")
+    @Mapping(source = "duracion", target = "duration")
+    @Mapping(source = "genero", target = "genre")
+    @Mapping(source = "fechaEstreno", target = "releaseDate")
+    @Mapping(source = "clasificacion", target = "rating")
+    MovieDto toDto(MovieEntity entity);
+    List<MovieDto> toDto(Iterable<MovieEntity> entities);
+}
