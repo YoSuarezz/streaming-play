6c03920c9a4db1221be6c45c335a5b0f815fa2b0
Sebastian <sebassuarez103@gmail.com>
2025-10-11 17:43:54 -0500

    Add validation for UpdateMovieDto and enhance exception handling in RestExceptionHandler

--- src/main/java/com/streaming/play/web/controller/MovieController.java
+++ src/main/java/com/streaming/play/web/controller/MovieController.java
@@ -5,6 +5,7 @@ import com.streaming.play.domain.dto.SuggestRequestDto;
 import com.streaming.play.domain.dto.UpdateMovieDto;
 import com.streaming.play.domain.service.MovieService;
 import com.streaming.play.domain.service.StreamingPlayAiService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -49,8 +50,7 @@ public class MovieController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<MovieDto> update(@PathVariable long id,@RequestBody UpdateMovieDto updateMovieDto) {
-
+    public ResponseEntity<MovieDto> update(@PathVariable long id,@RequestBody @Valid UpdateMovieDto updateMovieDto) {
         return ResponseEntity.ok(movieService.update(id, updateMovieDto));
     }
 
--- src/main/java/com/streaming/play/web/exception/RestExceptionHandler.java
+++ src/main/java/com/streaming/play/web/exception/RestExceptionHandler.java
@@ -2,8 +2,12 @@ package com.streaming.play.web.exception;
 
 import com.streaming.play.domain.exception.MovieAlreadyExistsException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
@@ -12,5 +16,24 @@ public class RestExceptionHandler {
     public ResponseEntity<Error> handleException(MovieAlreadyExistsException e) {
         Error error = new Error("movie-already-exists", e.getMessage());
         return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<Error>> handleException(MethodArgumentNotValidException e) {
+        List<Error> errors = new ArrayList<>();
+
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            String field = error.getField();
+            String message = error.getDefaultMessage();
+            errors.add(new Error(field, message));
+        });
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Error> handleException(Exception e) {
+        Error error = new Error("internal-server-error", e.getMessage());
+        return ResponseEntity.internalServerError().body(error);
     }
 }
